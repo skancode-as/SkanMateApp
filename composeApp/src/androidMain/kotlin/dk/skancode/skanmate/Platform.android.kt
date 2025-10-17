a05654a44723a67467e31cd0d3523f57f57a3167
TobiasTheDanish <t.hc199811@gmail.com>
2025-10-17 13:05:58 +0200

    feat(): implement image loading from android

--- composeApp/src/androidMain/kotlin/dk/skancode/skanmate/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/dk/skancode/skanmate/Platform.android.kt
@@ -1,11 +1,23 @@
 package dk.skancode.skanmate
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
 import com.russhwolf.settings.Settings
 import dk.skancode.skanmate.util.LocalCameraScanManager
 import dk.skancode.barcodescannermodule.compose.LocalScannerModule
+import androidx.core.net.toUri
+import java.io.ByteArrayOutputStream
 
 @Composable
 actual fun rememberScanModule(): ScanModule {
@@ -24,4 +36,23 @@ actual fun CameraView(
     cameraUi: @Composable BoxScope.(CameraController) -> Unit,
 ) {
     AndroidCameraView(cameraUi = cameraUi)
+}
+
+@Composable
+actual fun loadImageAsState(imagePath: String): State<Painter> {
+    val fallbackBitmap = ImageBitmap(1, 1)
+    val context = LocalContext.current
+
+    val bitmap: Bitmap? = remember(context, imagePath) {
+        val inputStream = context.contentResolver.openInputStream(imagePath.toUri())
+
+        if (inputStream == null) {
+            println("Could not find open input stream at imagePath: $imagePath")
+            null
+        }
+
+        BitmapFactory.decodeStream(inputStream)
+    }
+
+    return rememberUpdatedState(BitmapPainter(bitmap?.asImageBitmap() ?: fallbackBitmap))
 }
