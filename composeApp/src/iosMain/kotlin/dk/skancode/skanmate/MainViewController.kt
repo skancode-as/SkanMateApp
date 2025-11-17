113d2d653112192d5ea14a925de8807ae231653c
“TobiasTheDanish“ <t.hc199811@gmail.com>
2025-11-17 15:09:39 +0100

    feat(audio): add ios audio player implementation

--- composeApp/src/iosMain/kotlin/dk/skancode/skanmate/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/dk/skancode/skanmate/MainViewController.kt
@@ -3,6 +3,7 @@ package dk.skancode.skanmate
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -12,14 +13,24 @@ import dk.skancode.skanmate.ui.component.CameraBarcodeScanner
 import dk.skancode.skanmate.ui.component.LocalCameraScanManager
 import dk.skancode.skanmate.ui.viewmodel.CameraScanViewModel
 import dk.skancode.skanmate.util.CameraScanManagerImpl
+import dk.skancode.skanmate.util.LocalAudioPlayer
 
 private val cameraScanManager = CameraScanManagerImpl()
 
 fun MainViewController() = ComposeUIViewController {
+    val audioPlayer = IosAudioPlayer()
+
+    DisposableEffect(Unit) {
+        onDispose { audioPlayer.release() }
+    }
+
     val cameraPowerViewModel = viewModel { CameraScanViewModel(cameraScanManager) }
 
     val showCameraScanner by cameraPowerViewModel.cameraPowerState.collectAsState()
-    CompositionLocalProvider(LocalCameraScanManager provides cameraScanManager) {
+    CompositionLocalProvider(
+        LocalCameraScanManager provides cameraScanManager,
+        LocalAudioPlayer provides audioPlayer,
+        ) {
         Scaffold { padding ->
             App()
 
