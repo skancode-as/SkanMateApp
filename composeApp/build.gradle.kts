6bd97313a757917b3c5e7e8151077f9b679242da
“TobiasTheDanish“ <t.hc199811@gmail.com>
2025-11-25 13:07:43 +0100

    chore(build): bump android versions

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -77,8 +77,8 @@ android {
         applicationId = "dk.skancode.skanmate"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 16
-        versionName = "1.0.0-beta"
+        versionCode = 17
+        versionName = "1.0.0-beta.17"
         multiDexEnabled = true
     }
     packaging {
